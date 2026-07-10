3733aa9d45b3ef2e682d9de5f02a2182dcd66469
Laxman Gaidhankar <laxmangaidhankar44@gmail.com>
2026-07-10 13:50:38 +0530

    add: database conncetion updates and environmental variables

--- relaypay-backend/src/config/db.js
+++ relaypay-backend/src/config/db.js
@@ -1,2 +1,20 @@
 // MongoDB / Mongoose connection setup will live here.
-module.exports = {};
+const mongoose = require('mongoose');
+const env = require("./env");
+
+const MONGO_TEST_URI = process.env.MONGO_TEST_URI;
+
+
+async function connectToMongoDB() {
+  try {
+    await mongoose.connect(env.MONGO_TEST_URI);
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error("MongoDB Connection Failed:", err.message);
+    process.exit(1);
+  }
+}
+
+module.exports = {
+  connectToMongoDB
+}
--- relaypay-backend/src/config/env.js
+++ relaypay-backend/src/config/env.js
@@ -1,2 +1,21 @@
 // Environment loading and validation will live here.
-module.exports = {};
+const dotenv = require("dotenv");
+dotenv.config();
+
+const requiredEnvVariables = [
+  "PORT",
+  "MONGO_URI",
+  "MONGO_TEST_URI",
+];
+
+requiredEnvVariables.forEach((key) => {
+  if (!process.env[key]) {
+    throw new Error(`Missing required environment variable: ${key}`);
+  }
+});
+
+module.exports = {
+  PORT: process.env.PORT,
+  MONGO_URI: process.env.MONGO_URI,
+  MONGO_TEST_URI: process.env.MONGO_TEST_URI,
+};
