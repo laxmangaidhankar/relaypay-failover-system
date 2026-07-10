51b3736a8a35c5a1c8bc1def7f09cedfc0438053
Laxman Gaidhankar <laxmangaidhankar44@gmail.com>
2026-07-10 13:59:18 +0530

    errors handling code

--- relaypay-backend/src/utils/errors.js
+++ relaypay-backend/src/utils/errors.js
@@ -1,1 +1,45 @@
-module.exports = {};
+class InsufficientBalanceError extends Error {
+    constructor() {
+        super();
+
+        this.name = "InsufficientBalanceError";
+
+        this.statusCode = 400;
+    }
+}
+
+class WalletInactiveError extends Error {
+  constructor() {
+    super();
+
+    this.name = "WalletInactiveError";
+    this.statusCode = 400;
+  }
+}
+
+class DuplicateTransactionError extends Error {
+  constructor() {
+    super();
+
+    this.name = "DuplicateTransactionError";
+    this.statusCode = 400;
+  }
+}
+
+class ConcurrencyConflictError extends Error{
+  constructor(message = "Concurrency conflict detected") {
+    super(message);
+
+    this.name = "ConcurrencyConflictError";
+    this.statusCode = 400;
+  }
+}
+
+
+module.exports = {
+  InsufficientBalanceError,
+  WalletInactiveError,
+  DuplicateTransactionError,
+  ConcurrencyConflictError
+
+};
