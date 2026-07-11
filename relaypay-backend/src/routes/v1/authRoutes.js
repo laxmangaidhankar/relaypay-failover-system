feae46d6be0bcd3908b6d27eda45d5b077947354
Laxman Gaidhankar <laxmangaidhankar44@gmail.com>
2026-07-12 01:38:23 +0530

    add: authentication routes and updates naming convention

---
+++ relaypay-backend/src/routes/v1/authRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const authRouter = express.Router();
+
+const { register, login, refresh, logout } = require('../../controllers/authController');
+const { authRateLimiter } = require('../../middleware/rateLimiter');
+
+authRouter.post('/register', authRateLimiter, register);
+authRouter.post('/login', authRateLimiter,  login);
+authRouter.post('/refresh', refresh);
+authRouter.post('/logout', logout);
+
+
+module.exports = {
+  authRouter,
+}
